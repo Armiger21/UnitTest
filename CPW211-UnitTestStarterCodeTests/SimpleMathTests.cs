dcbaff250db709d6033a8f707a8d39e3023d996b
Armiger21 <Porfirdruid1@gmail.com>
2023-02-16 22:13:45 -0800

    Added Divide method and Subtract method

--- CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
+++ CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -19,14 +19,20 @@ namespace CPW211_UnitTestStarterCode.Tests
         public void Add_TwoNumbers_ReturnsSum(double num1, double num2)
         {
             // Use the DataRow values to test the Add method
-            Assert.Fail();
+            double result = SimpleMath.Add(num1, num2);
+            Assert.AreEqual(num1 + num2, result);
         }
 
         [TestMethod]
-        public void Multiply_TwoNumbers_ReturnsProduct()
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -10)]
+        [DataRow(0, 0)]
+        public void Multiply_TwoNumbers_ReturnsProduct(double num1, double num2)
         {
             // Use a few pairs of values to test the Multiply method
-            Assert.Fail();
+            double result = SimpleMath.Multiply(num1, num2);
+            Assert.AreEqual(num1 * num2, result);
         }
 
         [TestMethod]
@@ -34,11 +40,31 @@ namespace CPW211_UnitTestStarterCode.Tests
         {
             // Divide by zero should throw an argument exception with a message
             // "Denominator cannot be zero"
-            Assert.Fail();
+
+           Assert.ThrowsException<ArgumentException>(() => SimpleMath.Divide(1, 0));
         }
 
         // TODO: Test Divide method with two valid numbers
+        [TestMethod]
+        [DataRow(5, 10)]
+        [DataRow(10, 100)]
+        [DataRow(-1, -10)]
+        public void Divide_TwoNumbers_ReturnsProduct(double num2, double num1)
+        {
+            double result = SimpleMath.Divide(num1, num2);
+            Assert.AreEqual(num1 / num2, result);
+        }
 
         // TODO: Test subtract method with two valid numbers
+
+        [TestMethod]
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -10)]
+        public void Subtract_TwoNumbersReturnsProduct(double num1, double num2)
+        {
+            double result = SimpleMath.Subtract(num1, num2);
+            Assert.AreEqual(num1 - num2, result);
+        }
     }
 }
